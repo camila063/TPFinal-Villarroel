4ad3cda234b71e48d49d4b26ecd70c8121201071
camila063 <villarroelcamila49@gmail.com>
2025-02-04 15:04:27 -0300

    terminar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/principal/page/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'', component:InicioComponent,
+},
+{
+  path:"",loadChildren:()=>import('./modules/principal/page/page.module').then(m=>m.PagesModule)
+},
+{
+  path:'',loadChildren:()=>import('./modules/admin/admin.module').then(m=>m.AdminModule)
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
